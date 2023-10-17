ec5dc9d14db0fb6b845e12de6e509857b41bc474
Yoyisn <teodork789@outlook.com>
2023-10-17 14:11:31 -0500

    ALL PROYECT WORKING

--- Backend/Src/Models/user.model.js
+++ Backend/Src/Models/user.model.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+    client: {
+        type: String,
+        default: 'Usuario',
+    },
     username: {
         type: String,
         required: true,
