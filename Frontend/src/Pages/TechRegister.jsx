c04f9c0cced61b019b5259ea9e8c25221db68c36
LuisDaniel2 <delfus67.ldso@gmail.com>
2023-12-11 17:28:13 -0500

    tech register mejorado

--- Frontend/src/Pages/TechRegister.jsx
+++ Frontend/src/Pages/TechRegister.jsx
@@ -22,7 +22,7 @@ function TechRegisterPage() {
 
   return (
     <>
-      <h1 className="flex font-abc text-6xl translate-y-[140px] justify-center">
+      <h1 className="flex font-abc text-6xl translate-y-[110px] justify-center">
         {" "}
         <Link to="/"> ServiTech </Link>{" "}
         <img
@@ -33,8 +33,10 @@ function TechRegisterPage() {
       </h1>
 
       <div className="shadow-inner hover:shadow-lg px-5 shadow-black hover:shadow-black py-5 mt-10 rounded-md bg-Darkredlow">
+
         <div className="flex h-[calc(100vh-100px)] items-center justify-center">
-          <div className="bg-Bgblue max-w-md p-10 shadow-inner hover:shadow-lg shadow-black hover:shadow-black mt-10 rounded-md">
+
+          <div className="bg-Bgblue max-w-md p-10 shadow-inner hover:shadow-lg shadow-black hover:shadow-black mt-10 rounded-md overflow-y-scroll max-h-[650px]">
             {registerErrors.map((error, i) => {
               <div key={i} className="bg-Softorange p-2 text-black text-center">
                 {" "}
@@ -50,13 +52,13 @@ function TechRegisterPage() {
               })}
             >
               <input
-                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2 hover:list-disc "
                 type="text"
                 placeholder="Nombre Completo"
                 {...register("name", { required: true })}
               />
               {errors.name && (
-                <p className="text-Softorange"> Your name is required </p>
+                <p className="text-Softorange"> Tu nombre es requerido </p>
               )}
 
               <input
@@ -66,7 +68,7 @@ function TechRegisterPage() {
                 {...register("email", { required: true })}
               />
               {errors.email && (
-                <p className="text-Softorange"> Email is required </p>
+                <p className="text-Softorange"> El Email es requerido </p>
               )}
 
               <input
@@ -76,7 +78,7 @@ function TechRegisterPage() {
                 {...register("password", { required: true })}
               />
               {errors.password && (
-                <p className="text-Softorange"> Password is required </p>
+                <p className="text-Softorange"> Contrasena es requerida </p>
               )}
 
               <input
@@ -87,15 +89,106 @@ function TechRegisterPage() {
                 {...register("number", { required: true })}
               />
               {errors.number && (
-                <p className="text-Softorange"> Number is required </p>
+                <p className="text-Softorange"> El numero es requerido </p>
               )}
 
-              <label> Sube tu hoja de vida </label>
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="text"
+                placeholder="Direccion de residencia"
+                {...register("direccion", { required: true })}
+              />
+              {errors.number && (
+                <p className="text-Softorange">La direccion es requerida </p>
+              )}
+
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="number"
+                placeholder="Codigo postal"
+                maxLength={6}
+                {...register("Codigo postal", { required: true })}
+              />
+              {errors.number && (
+                <p className="text-Softorange"> El codigo postal es requerido </p>
+              )}
+
+              <input
+                className="w-full  bg-Grayishblue text-Darkgrayishblue px-4 py-2 rounded-md my-2"
+                type="date"
+                placeholder="Fecha de nacimiento"
+
+                {...register("Fecha nacimiento", { required: true })}
+              />
+              {errors.number && (
+                <p className="text-Softorange"> Tu fecha de nacimiento es requerida </p>
+              )}
+
+
+              <label> Sube tu certificado de basica Primaria </label>
               <input
                 className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
                 type="file"
-                {...register("file")}
+                {...register("file-Primaria")}
               />
+
+              <label> Sube tu certificado de basica Secundaria </label>
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="file"
+                {...register("file-Secundaria")}
+              />
+
+              <label> Sube tu certificado de estudios Tecnico o Tecnologo </label>
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="file"
+                {...register("file-Tecnico-Tecnologo")}
+              />
+
+              <label> Sube tu certificado de estudios Universitarios  </label>
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="file"
+                {...register("file-Universidad")}
+              />
+
+              <label> Sube tu certificado de estudios de idiomas opcional </label>
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="file"
+                {...register("file-Idiomas")}
+              />
+
+              <label> Sube tu certificado de antecedentes penales  </label>
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="file"
+                {...register("file-Antecedentes")}
+              />
+
+              <label> Tienes alguna discapacidad? opcional  </label>
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="file"
+                {...register("file-Discapacidad")}
+              />
+
+              <label> Sube tu certificados o historial de trabajos realizados  </label>
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="file"
+                {...register("file-Trabajos-realizados")}
+              />
+
+              <label> Sube tu certificado de trabajos anteriores  </label>
+              <input
+                className="w-full  bg-Grayishblue text-black px-4 py-2 rounded-md my-2"
+                type="file"
+                {...register("file-Extrabajos")}
+              />
+
+
 
               <button className="bg-Grayishblue hover:bg-Brown hover:text-Grayishblue text-black px-4 py-2 rounded-md my-2">
                 {" "}
